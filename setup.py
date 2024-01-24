ddb374ce9094e8765852212941d7d0621e862ce6
Mukesh441 <sethi.mukeshjain@gmail.com>
2024-01-24 15:02:36 +0530

    Langchain-openai is used and its updated in Rquirements.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Mukesh Sethi',
     author_email='sethi.mukeshjain@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["openai","langchain-openai","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
